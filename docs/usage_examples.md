4cb0507664518b082fde978ae10693f4b1b46b5b
Lee-Way Wang <lee.way56@comcast.net>
2021-11-25 14:31:17 -0800

    Create usage examples, unit tests, move scripts
    - Create usage examples document
    - Create unit tests document
    - Move scripts to different location

---
+++ docs/usage_examples.md
@@ -0,0 +1,5 @@
+# Usage Examples
+
+## Convolution
+
+## Matrix Multiplication
---
+++ tests/unit_tests.jl
@@ -0,0 +1,11 @@
+using Test
+
+
+@testset "Convolution" begin
+    @test 1 == 1
+end
+
+
+@testset "Matrix multiplication" begin
+    @test 1 == 1
+end
